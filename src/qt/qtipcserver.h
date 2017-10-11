ca706b80d252b98f7da036a2be95f7638d0235a5
Findblocks.com <findblocks@gmail.com>
2017-10-11 10:16:22 +0100

    uploaded

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define Fishing-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "FishingURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
